7b8cf7e4c61176397007642a4bcb2fa746d7066e
internaru <internaru@hanmail.net>
2025-01-22 02:04:25 +0900

    * 키보드 제외한 메인 화면구성 LinearLayout으로 변경 * 위 변경시 시스템 네비게이션바 아래 표시됨 -> 숨기기 위한 함수 오버로딩 * 타이틀바 사이즈 축소

--- app/src/main/java/com/shlee/inappkeyboard/MainActivity.kt
+++ app/src/main/java/com/shlee/inappkeyboard/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.shlee.inappkeyboard
 
+import android.os.Build
 import android.os.Bundle
 import android.text.InputType
 import android.view.View
+import android.view.WindowInsets
+import android.view.WindowInsetsController
 import android.view.inputmethod.EditorInfo
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
@@ -22,5 +25,23 @@ class MainActivity : AppCompatActivity() {
         // pass the InputConnection from the EditText to the keyboard
         val ic = editText.onCreateInputConnection(EditorInfo())
         keyboard.setInputConnection(ic)
+    }
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        if(!hasFocus) return
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            window.insetsController?.hide(WindowInsets.Type.systemBars() or WindowInsets.Type.navigationBars())
+            window.insetsController?.systemBarsBehavior = WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+        else {
+            window.decorView.systemUiVisibility =
+                (View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or
+                        View.SYSTEM_UI_FLAG_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_HIDE_NAVIGATION or
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
+                        View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+                        View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
+        }
     }
 }
