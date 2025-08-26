39983bc3a76342eeef79258e0128feb9fff13158
internaru <internaru@hanmail.net>
2025-08-26 23:45:39 +0900

    * 네트웍 연결상태를 메인화면 활성화시 표시   > 초록 : 연결 OK   > 빨강 : 연결 NG

--- app/src/main/java/com/shlee/inappkeyboard/MainActivity.kt
+++ app/src/main/java/com/shlee/inappkeyboard/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.shlee.inappkeyboard
 
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.os.Build
 import android.os.Bundle
 import android.text.InputType
@@ -7,8 +9,16 @@ import android.view.View
 import android.view.WindowInsets
 import android.view.WindowInsetsController
 import android.view.inputmethod.EditorInfo
+import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,6 +52,39 @@ class MainActivity : AppCompatActivity() {
                         View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
                         View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
                         View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION)
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+
+        checkInternet(this) { isConnected ->
+            if (isConnected) {
+                showCustomToast(this, "Network is available", 26f, "GREEN") // 연결됨
+            } else {
+                showCustomToast(this, "Network is not available", 26f, "RED")   // 연결 안됨
+            }
+        }
+    }
+
+    // 네트워크 체크 함수
+    fun checkInternet(context: Context, onResult: (Boolean) -> Unit) {
+        CoroutineScope(Dispatchers.IO).launch {
+            val result = try {
+                val url = URL("https://clients3.google.com/generate_204")
+                val connection = url.openConnection() as HttpURLConnection
+                connection.setRequestProperty("User-Agent", "Android")
+                connection.setRequestProperty("Connection", "close")
+                connection.connectTimeout = 1500
+                connection.connect()
+                connection.responseCode == 204
+            } catch (e: Exception) {
+                false
+            }
+
+            // 메인(UI) 스레드로 결과 전달
+            withContext(Dispatchers.Main) {
+                onResult(result)
+            }
         }
     }
 }
